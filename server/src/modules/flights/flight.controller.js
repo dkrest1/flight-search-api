71039767ffb9f79e033f8f9ee10716a23da741bd
Akande Oluwatosin <aktosin1@gmail.com>
2023-06-18 19:15:20 +0100

    feat: sort

--- server/src/modules/flights/flight.controller.js
+++ server/src/modules/flights/flight.controller.js
@@ -1,6 +1,5 @@
 import { flightInfoService } from "./flight.service.js";
 import { logger } from "../../config/logger.config.js";
-import { validateFlightInput } from "./validators/flight.validator.js";
 
 
 
@@ -9,14 +8,15 @@ import { validateFlightInput } from "./validators/flight.validator.js";
 //@route : /flight/search
 //@access: public
 export const searchFlightInfo = async (req, res) => {
-    const { origin, destination, depature_date, return_date, passenger_number } = await validateFlightInput(req.body);
+    //get request body
+    const { origin, destination, depature_date, return_date, passenger_number } = req.body
     try {
         const result = await flightInfoService(origin, destination, depature_date, return_date, passenger_number);
         return res.status(201).json({ success: true, data: result });
     } catch (err) {
         logger.error(err)
         return res.status(400).json({
-            success: failure,
+            success: false,
             status: err.code,
             message: err.description,
 
@@ -26,37 +26,44 @@ export const searchFlightInfo = async (req, res) => {
 }
 
 //@description: filter flight info 
-//@route : /flight/search?filter=query
+//@route : /flight/search?filterBy=airline||arrival||departure||duration||price
+//@route : /flight/search?soryBy=departure||duration||price&orderBy=asc||desc
 //@access: public
-export const filterFlightResult = async (req, res) => {
-    // get query
-    const query = Object.values(req.query)[0];
-    const validQueries = ["airline", "departure", "arrival", "duration", "price"];
-    const validatedQuery = validQueries.includes(query)
-    if (!validatedQuery) {
-        return res.status(400).json({ success: false, message: "invalid query" })
+export const filterAndSortFlightResult = async (req, res) => {
+    //get request body
+    const { origin, destination, depature_date, return_date, passenger_number } = req.body
+    //get query
+    const queryKeys = Object.keys(req.query)
+    // get query values
+    const queryValues = Object.values(req.query);
+
+    //validate query keys
+    const validQueriesKeys = ["filterBy", "sortBy", "orderBy"];
+    const validatedQueryKeys = queryKeys.every(value => validQueriesKeys.includes(value))
+
+    //validate query values
+    const validQueriesValues = ["airline", "arrival", "departure", "duration", "price", "asc", "desc"];
+    const validatedQueryValues = queryValues.every(value => validQueriesValues.includes(value))
+
+    if (!validatedQueryKeys || !validatedQueryValues) {
+        return res.status(400).json({ success: false, message: "invalid search" })
     }
-    const { origin, destination, depature_date, return_date, passenger_number } = await validateFlightInput(req.body);
+
+
+
     try {
         const flightResult = await flightInfoService(origin, destination, depature_date, return_date, passenger_number);
-        // filter flight result  by query
-        const filteredResult = await flightResult.map((result) => {
-            return {
-                id: result.id,
-                [query]: result[query]
-            }
-        })
+        //filter rsult
+        if (req.query === "filterBy" && req.query === "orderBy") {
+            return res.status(200).json({ message: "great" })
+        }
 
-        return res.status(200).json({ success: true, data: filteredResult })
+
+        //sort flight 
+
+
     } catch (error) {
         logger.error(error)
         return res.status(500).json({ success: false, message: "something went wrong, try again later" })
     }
-}
-
-//@description: sort flight info 
-//@route : /flight/search?sort=query&asc
-//@access: public
-export const sortFlightInfo = async () => {
-    return
 }
