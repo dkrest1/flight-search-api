cdb13cf83ae45fb88b5c58c80b0d890699bdf334
Akande Oluwatosin <aktosin1@gmail.com>
2023-06-26 13:09:42 +0100

    feat: get bookings

--- server/src/modules/flights/flight.route.js
+++ server/src/modules/flights/flight.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 import auth from "../middleware/auth.middleware.js";
-import { searchFlightController, comfirmFlightController, bookFlightController } from "./flight.controller.js"
+import { searchFlightController, comfirmFlightController, bookFlightController, getBookingsController } from "./flight.controller.js"
 
 const router = new express.Router();
 
 router.post("/search", searchFlightController);
-router.post("/comfirm/:id", comfirmFlightController);
-router.post("/book/:id", bookFlightController);
+router.post("/comfirm/:id", auth, comfirmFlightController);
+router.post("/book/:id", auth, bookFlightController);
+router.get("/bookings", auth, getBookingsController);
 export default router
--- server/src/modules/flights/model/flight.model.js
+++ server/src/modules/flights/model/flight.model.js
@@ -2,14 +2,17 @@ import mongoose from "mongoose";
 
 const BookingSchema = mongoose.Schema(
     {
-        username: { type: String, required: true },
-        email: { type: String, required: true, unique: true, },
-        password: { type: String, required: true },
+        fullname: { type: String, required: true },
+        email: { type: String, required: true, },
+        bookId: { type: String, required: true, unique: true },
+        queueOfficeId: { type: String, required: true },
+        reference: { type: String, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
     },
+
     {
         timestamps: true
     }
 )
-
 
 export default mongoose.model("Booking", BookingSchema)
