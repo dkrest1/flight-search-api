6cc8e6eb1733def4cb9736772e6880add2f2f406
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-06-27 15:02:37 +0100

    worked on the details page

---
+++ client/src/Components/Details.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div className='w-full bg-white h-full'>
+        <div className=' grid grid-cols-3 mt-10 gap-5 items-between px-5'>
+            <div className=' col-span-2 w- flex flex-col bg-green-50 pt-2'>
+                <div className='flex flex-row justify-center border-b pb-1 border-slate-400'>
+                    <h3 className='text-sm font-medium'>Passenger Details</h3>
+                </div>
+                <div className='flex flex-col border rounder-lg mt-3 bg-white w-full'>
+                    <h5 className='text-sm font-semibold border-b '>Passenger 1</h5>
+                    <div className='flex flex-row w-full gap-3 mt-4 mb-5'>
+                        <div className=' w-[20%] flex flex-row border rounded divide-x text-xs'>
+                            <p className='bg-blue-950 px-1 text-white'>MR</p>
+                            <p className='px-1'>MRS</p>
+                        </div>
+                        <div className='w-[80%] flex flex-row text-xs border rounded divide-x '>
+                            <p className='w-[50%]'>John </p>
+                            <p>Doe</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col border rounder-lg mt-3 bg-white w-full'>
+                    <h5 className='text-sm border-b '>Contact Information</h5>
+                    <div className='flex flex-row w-full gap-3 mt-4 mb-5'>
+                        <div className='w-full flex flex-row text-xs border rounded divide-x '>
+                            <p className='w-[50%]'>123456789 </p>
+                            <p>Johndoe1985@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div className=' flex flex-row justify-center'>
+                    <button className='bg-blue-950 text-white text-xs font-semibold px-7 py-1'>Confirm</button>
+                </div>
+            </div>
+            <div className='w-full'>
+                <div className='w-full flex flex-col rounded border'>
+                    <h5 className='text-sm font-semibold border-b'>Fee Summary</h5>
+                    <div className='flex flex-row justify-between border-b'>
+                        <p>Fare</p>
+                        <p>$1500</p>
+                    </div>
+                    <div className='flex flex-row justify-between border-b'>
+                        <p>Tax</p>
+                        <p>$200</p>
+                    </div>
+                    <div className='flex flex-row justify-between border-b'>
+                        <p>Other charges</p>
+                        <p>$80</p>
+                    </div>
+                    <div className='flex flex-row justify-between border-b'>
+                        <p>Total Fare</p>
+                        <p>$1500 * 2</p>
+                    </div>
+                </div>
+                <div className=' flex flex-row justify-between text-xs'>
+                    <p>10% off</p>
+                    <p>$2700</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Details
--- client/src/Components/Flights.jsx
+++ client/src/Components/Flights.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faFilter, faPlane, faSort, faUser} from '@fortawesome/free-solid-svg-icons'
 
 function Flights() {
   return (
@@ -45,16 +47,65 @@ function Flights() {
           <button className='bg-blue-950 text-sm text-slate-200 py-2 px-3 rounded-lg'>SEARCH</button>
         </div>
       </div>
-      <div className='w-full mt-10 flex flex-row justify-between'>
-        <div>
-          <select name='sort-by'>
-            <option value='sort-by' className=''>SORT BY</option>
+      <div className='w-[85%] mt-10 flex flex-row justify-between'>
+        <div className='rounded-lg border'>
+          <select name='sort-by' className='text-[11px] text-slate-400'>
+            <option value='sort-by' className='text-xs text-red-600'>SORT BY</option>
+            <option value='name'  className=''>name</option>
+            <option value='price'  className=''>price</option>
+            <option value='origin'  className=''>origin</option>
+            <option value='destination'  className=''>Destination</option>
           </select>
         </div>
-        <div className='flex flex-row divide'>
-          <button>a</button>
-          <button>b</button>
+        <div className='flex flex-row divide-x-2 divide-slate-300'>
+          <button><FontAwesomeIcon icon={faSort} className='px-1 text-[16px] text-blue-950' /></button>
+          <button className='px-1'>
+            <div className='bg-blue-950 rounded'>
+              <FontAwesomeIcon icon={faFilter} className='px-1 text-[12px] text-white' />
+            </div>
+          </button>
         </div>
+      </div>
+      {/* Dynamic search data from server */}
+      <div className='w-[85%] mt-3 h-20'>
+        <div className='w-full flex flex-row rounded-lg divide--2 border-2 shadow-sm h-full items'>
+          <div className='w-[14%] border-r border-slate-300'>
+            <img src=''alt='' className='w-[95%] h-full py-2'/>
+          </div>
+          <div className='flex flex-row border-dashed border-r border-slate-300 h-full items-center justify-center w-[43%]'>
+            <div className='flex flex-col mr-5'>
+              <h2 className='font-semibold text-xl tracking-wide '>LOS</h2>
+              <small className='text-[8px]'>12:20 AM</small>
+            </div>
+            <div className='flex flex-col items-center gap-2 -mt-2'>
+              <small className='text-[8px]'>15h 40m</small>
+              <FontAwesomeIcon icon={faPlane} className='text-sm font-thin'/>
+              <small className='text-[8px]'>2 Stops</small>
+            </div>
+            <div className='flex flex-col mx-5'>
+              <p className='font-semibold text-xl tracking-wide'>TYO</p>
+              <small className='text-[8px]'>04:00 AM</small>
+            </div>
+          </div>
+          <div className='flex flex-row w-[43%] justify-end items-center gap-10 py3 mr-3'>
+            <div className='flex flex-col'>
+              <small className='text-[8px]'>price per seat</small>
+              <h3 className='text-xl font-semibold tracking-wide -ml-2'>$2400</h3>
+                <small className='text-[8px]'>
+                  <FontAwesomeIcon icon={faUser} /> First Class</small>
+            </div>
+            <div className='flex flex-col'>
+              <button className='bg-blue-950 text-slate-200 text-[10px] w-fit py-1 px-2 rounded'>Book Now</button>
+              <small className='text-[8px]'>10/300 Available Seats</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Pagination */}
+      <div className='flex flex-row mt-6 gap-3'>
+        <button className='bg-gray-300 px-2 rounded'>PREV</button>
+        <p>1</p>
+        <button className='border-2 rounded px-2 border-gray-300 text-sm'>NEXT</button>
       </div>
     </div>
   )
