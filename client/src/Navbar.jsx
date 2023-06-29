1ece8be109596912f31557791fb6f4a688d9b950
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-06-29 11:08:17 +0100

    Added logo to navbar, modified home page, flight page

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-// import heroImg from '../public/Assets/hero-img.jpg'
-
+import logo from '../public/Assets/flight-logo.png'
 function Navbar() {
   return (
         <nav className='flex flex-row w-full py-4 px-10'>
             <div className='flex flex-row w-full justify-between '>
-                <NavLink to='/' className='flex flex-row text-white gap-2 font-medium'>
-                    <img src='' alt='logo'/>
-                    <h6>Travel</h6>
+                <NavLink to='/' className='flex flex-row text-white gap-2 font-semibold text-xl'>
+                    <img src={logo} alt='logo' className='absolute w-24 h-24 -mt-8 -ml-9'/>
+                    <h6 className='ml-8 '>Travel</h6>
                 </NavLink>
-                <nav className='flex flex-row gap-5 text-white font-medium'>
+                <nav className='flex flex-row gap-5 text-white font-medium text-lg'>
                     <NavLink to ='/flights' end className={({ isActive, isPending }) =>isPending ? "pendinglink" : isActive ? "activelink" : "pendinglink"}>Flights</NavLink>
                     <NavLink to ='/bookings' end
                         className={({ isActive, isPending }) =>isPending ? "pendinglink hover:text-lg" : isActive ? "activelink" : " pendinglink"}
