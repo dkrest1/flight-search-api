7eaac6f620c206785ffaa59237e8f5c0ac4899db
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-06-29 05:53:21 +0100

    worked on login and sign up page

--- client/src/Components/Bookings.jsx
+++ client/src/Components/Bookings.jsx
@@ -1,8 +1,53 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlane } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 const Bookings = () => {
   return (
-    <div>Bookings</div>
+    <div className='h-full bg-white flex flex-col'>
+      <div className='mt-10 flex flex-row w-full gap-5'>
+        <div className='flex flex-col w-[50%]'>
+          <div className='flex flex-row w-[full] border-dashed border-b justify-between items-center px-2 bg-red-600 py-1'>
+            <div className='flex flex-col justify-center rounded-sm bg-white'>
+              <div className='flex flex-col justify-center h-7 w-7 rounded-full bg-red-600'>
+                <p className='text-[9px] text-slate-200 italic'>Airfare</p>
+              </div>
+            </div>
+            <FontAwesomeIcon icon={faPlane} className='text-sm text-white'/>
+          </div>
+          <div className='flex flex-col mt-3 px-4'>
+            <div className='flex flex-col'>
+              <h5 className='text-[12px] font-medium'>Name</h5>
+              <p className='text-[12px]'>Mr John Doe</p>
+            </div>
+            <div>
+              <div className=' grid grid-cols-3 font-medium text-[12px] mt-3'>
+                <h5>From</h5>
+                <h5>Date</h5>
+                <h5>Carrier</h5>
+              </div>
+              <div className='grid grid-cols-3  text-[12px]'>
+                <p>Lagos, Nigeria</p>
+                <p>22/06/23</p>
+                <p>D7</p>
+              </div>
+            </div>
+            <div>
+              <div className=' grid grid-cols-3 font-medium text-[12px] mt-3'>
+                <h5>To</h5>
+                <h5>Time</h5>
+                <h5>Carrier</h5>
+              </div>
+              <div className='grid grid-cols-3  text-[12px]'>
+                <p>Tokyo, Japan</p>
+                <p>04:00</p>
+                <p>8A</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/Components/Signup.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Signup = () => {
+  return (
+    <div className='h-full w-full flex flex-col justify-center items-center'>
+        <div className='flex flex-col items-center bg-white w-[28%] px-8 pt-10 pb-5 rounded-md'>
+            <h3 className='font-medium '>Create an account</h3>
+            <p className='text-[10px] text-slate-500'>Enter your details to create your account</p>
+            <form>
+                <div className='grid grid-cols-2 gap-x-6 mt-5'>
+                    <label htmlFor='first-name' className='text-xs text-slate-400'>First Name</label>
+                    <label htmlFor='last-name'className='text-xs text-slate-400'>Surname</label>
+                    <input
+                        className='text-sm border-t border-x border-b-2 border-b-slate-300 rounded-lg shadow-lg py-2 px-1' 
+                        type='text'
+                        name='first-name'
+                        placeholder='John'
+                        required
+                        />
+                    <input
+                        className='text-sm border-t border-x border-b-2 border-b-slate-300 rounded-lg shadow-lg py-2 px-1' 
+                        type='text'
+                        name='surname'
+                        placeholder='Doe'
+                        required
+                        />
+                </div>
+                <div className='flex flex-col mt-2'>
+                    <label htmlFor='email' className='text-xs text-slate-400'>Email</label>
+                    <input
+                        className='text-sm  border-t border-x border-b-2 border-b-slate-300 rounded-lg shadow-lg py-2 px-1'
+                        type='email'
+                        placeholder='Johndoe123@gmail.com'
+                        required
+                    />
+                </div>
+                <div className='flex flex-col mt-2'>
+                    <label htmlFor='password' className='text-xs text-slate-400'>Password</label>
+                    <input
+                        className='text-sm  border-t border-x border-b-2 border-b-slate-300 rounded-lg shadow-lg py-2 px-1'
+                        type='password'
+                        placeholder='Johndoe123@gmail.com'
+                        required
+                    />
+                </div>
+                <p className='text-[11px] mt-3'>
+                    <input type='checkbox'/> I agree to the COVID-19 <Link to='#' className='text-blue-600'>Disclaimer</Link> and Terms & Conditions
+                </p>
+                <button className='bg-blue-950 w-full mt-3 text-center text-white font-medium text-sm py-2 border-t border-x border-b-2 border-b-slate-300 rounded-md shadow-lg px-1'>Sign Up</button>
+            </form>
+            <div className='flex flex-row w-full gap-2 items-center mt-3'>
+                <div className='border border-gray-200 h-[2px] w-full'></div>
+                <p className='text-sm'>OR</p>
+                <div className='border border-gray-200 w-full'></div>
+            </div>
+            <button className='w-full text-sm mt-5 border-t border-x border-b-2 border-b-slate-300 rounded-lg shadow-lg py-2 px-1'><i className='fab fa-google text-red-600 '></i> Sign up with Google
+            </button>
+            <p className='text-sm mt-3 text-slate-600 font-medium'> Already have an account? <Link to='/login' className='text-blue-600 '>Login</Link></p>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
