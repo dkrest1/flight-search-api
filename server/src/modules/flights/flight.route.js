21674730a1e60cdaf959406be55e057469dbbcf1
Akande Oluwatosin <aktosin1@gmail.com>
2023-07-04 14:11:34 +0100

    feat: implemented rate limiting

--- server/src/modules/flights/flight.route.js
+++ server/src/modules/flights/flight.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 import auth from "../middleware/auth.middleware.js";
 import { searchFlightController, comfirmFlightController, bookFlightController, getBookingsController } from "./flight.controller.js"
+import { flightRateLimiter } from "../middleware/rate-limit.middleware.js";
 
 const router = new express.Router();
 
-router.post("/search", searchFlightController);
-router.post("/comfirm/:id", auth, comfirmFlightController);
-router.post("/book/:id", auth, bookFlightController);
+router.post("/search", flightRateLimiter, searchFlightController);
+router.post("/comfirm/:id", auth, flightRateLimiter, comfirmFlightController);
+router.post("/book/:id", auth, flightRateLimiter, bookFlightController);
 router.get("/bookings", auth, getBookingsController);
 
 export default router;
