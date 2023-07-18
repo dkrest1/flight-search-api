507fc941d93bf35576110d87f93e980bb774f064
Oluwatosin Akande <aktosin1@gmail.com>
2023-07-18 06:23:22 +0100

    fix: redis error

--- server/src/config/redis.config.js
+++ server/src/config/redis.config.js
@@ -1,7 +1,12 @@
 import { createClient } from "redis";
 import chalk from "chalk";
 
-export const redisClient = createClient();
+
+export const redisClient = createClient({
+    socket: { keepAlive: 10000000 },
+    url: process.env.URL,
+    database: 0
+});
 
 redisClient.on('connect', () => {
     console.log(chalk.red("Redis connected successfully "));
