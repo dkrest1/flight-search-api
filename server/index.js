922f8455b88954ec0296103d5edded531623d130
Oluwatosin Akande <aktosin1@gmail.com>
2023-07-18 10:17:34 +0100

    fixed up the ENV URL

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: process.env.WEB_URL,
             },
         ],
     },
